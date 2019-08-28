2120d6f7a01a48e5341e2ade90411db536da20d7
J0eyZHOU <44006730+J0eyZHOU@users.noreply.github.com>
2019-08-28 01:06:02 -0700

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,10 @@
+birth_year = input(" birth year")
+age = 2019 - int(birth_year)
+print(age)
+
+weight_lbs = input("whats your weight in pounds")
+weight_kg = float(weight_lbs) *0.45
+print(weight_kg)
+
+name = ' jennifer '
+print (name[1:-1])
